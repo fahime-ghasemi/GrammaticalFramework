f4fa2b6ba8d22b1f3e91116f603d0009a1bb4e31
fahime-ghasemi <fahime.s.ghasemi@gmail.com>
2015-09-03 23:00:19 +0430

    Open a window when click on setting

--- src/com/ikiu/tagger/controller/MenuSection.java
+++ src/com/ikiu/tagger/controller/MenuSection.java
@@ -4,8 +4,19 @@
  */
 package com.ikiu.tagger.controller;
 
-import java.awt.event.*;
-import javax.swing.*;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 
 /**
  *
@@ -44,7 +55,54 @@ class MenuSection implements ActionListener {
     }
     @Override
     public void actionPerformed(ActionEvent e) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+
+        if(e.getSource()==Setting)
+        {
+            JFrame fileFrame = new JFrame();
+            JButton btnSelectDirectory = new JButton("Select Directory");
+            JButton btnSaveConfig=new JButton("Save");
+            JTextField txtWorkspace = new JTextField(20);
+
+            fileFrame.setLayout(new FlowLayout());
+            JLabel fileLabel = new JLabel("Workspace :");
+
+            btnSelectDirectory.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    if(e.getSource()==btnSelectDirectory)
+                    {
+                        JFileChooser jFileChooser = new JFileChooser();
+                        jFileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                        int returnVal=jFileChooser.showOpenDialog(fileFrame);
+                        if(returnVal ==JFileChooser.APPROVE_OPTION)
+                        {
+                            java.io.File file = jFileChooser.getSelectedFile();
+                            txtWorkspace.setText(file.getPath());
+
+                        }
+
+                    }
+                }
+            });
+
+            btnSaveConfig.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    fileFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                    fileFrame.dispose();
+
+                }
+            });
+
+            fileFrame.add(fileLabel);
+            fileFrame.add(txtWorkspace);
+            fileFrame.add(btnSelectDirectory);
+            fileFrame.add(btnSaveConfig);
+
+            fileFrame.setVisible(true);
+            fileFrame.setSize(500, 200);
+        }
     }
 
 }
