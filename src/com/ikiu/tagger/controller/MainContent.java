44625c67bb02cc78cee29ab5d25a07457de62e49
fahime-ghasemi <fahime.s.ghasemi@gmail.com>
2015-09-11 09:38:05 +0430

    Use TextArea for utf-8 bug

--- src/com/ikiu/tagger/controller/MainContent.java
+++ src/com/ikiu/tagger/controller/MainContent.java
@@ -1,39 +1,46 @@
 package com.ikiu.tagger.controller;
 
-import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
-
+import java.awt.Font;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.swing.JComponent;
 import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 
 /**
  * Created by fahime on 9/4/15.
  */
 public class MainContent {
-    RSyntaxTextArea syntaxTextArea;
+    JTextArea textArea;
     public JComponent getComponent()
     {
-        syntaxTextArea = new RSyntaxTextArea(35,50);
-        JScrollPane scrollPane = new JScrollPane(syntaxTextArea);
+         textArea = new JTextArea();
+        Font font=textArea.getFont();
+
+        textArea.setFont(font.deriveFont(20f));
+        JScrollPane scrollPane = new JScrollPane(textArea);
+
         return scrollPane;
     }
 
     public void setTextAreaContent(String filesystemPath) {
 
         try {
-            FileReader reader = new FileReader(filesystemPath);
-            BufferedReader bufferedReader = new BufferedReader(reader);
-            syntaxTextArea.read(bufferedReader,null);
+            InputStreamReader inputStreamReader = new InputStreamReader(new FileInputStream(new File(filesystemPath)),"UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            textArea.read(bufferedReader, null);
             bufferedReader.close();
-            reader.close();
+            inputStreamReader.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+
         }
     }
 }
